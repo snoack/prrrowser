a19e738412dee562bb68833a2233bbe3ae687c2e
Sebastian Noack <sebastian.noack@gmail.com>
2018-06-23 19:33:41 -0400

    Fall back to thumbnail when loading orignal image fails

--- background.js
+++ background.js
@@ -13,51 +13,58 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) =>
         let parser = new DOMParser();
         let doc = parser.parseFromString(source, "text/html");
         let meta = doc.querySelector(".rg_meta");
-        return JSON.parse(meta.textContent).ou;
-      })
-      .catch(() => null);
+        return [JSON.parse(meta.textContent).ou,
+                doc.querySelector("#search img").src];
+      });
 
     cache.set(msg.keywords, promise);
   }
 
   promise
-    .then(url =>
+    .then(urls => new Promise((resolve, reject) =>
     {
-      if (!url)
-        return null;
-
-      return new Promise((resolve, reject) =>
+      let image = new Image();
+      image.addEventListener("load", () =>
       {
-        let image = new Image();
-        image.addEventListener("load", () =>
-        {
-          let {width, height} = msg;
-          let ratio = image.width / image.height;
-          if (ratio > width / height)
-            height = width / ratio;
-          else
-            width = height * ratio;
-
-          let canvas = document.createElement("canvas");
-          canvas.width = msg.width;
-          canvas.height = msg.height;
-          canvas.getContext("2d").drawImage(
-            image,
-            0,
-            0,
-            image.width,
-            image.height,
-            (msg.width - width) / 2,
-            (msg.height - height) / 2,
-            width,
-            height
-          );
-
-          resolve(canvas.toDataURL("image/png"));
-        });
-        image.addEventListener("error", reject);
-        image.src = url;
+        resolve(image);
       });
+      image.addEventListener("error", () =>
+      {
+        let thumbnail = new Image();
+        thumbnail.addEventListener("load", () =>
+        {
+          resolve(thumbnail);
+        });
+        thumbnail.addEventListener("error", reject);
+        thumbnail.src = urls[1];
+      });
+      image.src = urls[0];
+    }))
+    .then(image =>
+    {
+      let {width, height} = msg;
+      let ratio = image.width / image.height;
+      if (ratio > width / height)
+        height = width / ratio;
+      else
+        width = height * ratio;
+
+      let canvas = document.createElement("canvas");
+      canvas.width = msg.width;
+      canvas.height = msg.height;
+      canvas.getContext("2d").drawImage(
+        image,
+        0,
+        0,
+        image.width,
+        image.height,
+        (msg.width - width) / 2,
+        (msg.height - height) / 2,
+        width,
+        height
+      );
+
+      return canvas.toDataURL("image/png");
     })
     .catch(() => null)
     .then(sendResponse);
