6bf2cff23a6a158c335155b63a353ec0239f070f
Sebastian Noack <sebastian.noack@gmail.com>
2018-06-26 22:27:12 +0200

    Split logic into functions

--- background.js
+++ background.js
@@ -4,6 +4,96 @@ const DEFAULT_TOPIC = "cats";
 let cache = new Map();
 let topic, whitelist;
 
+function loadImage(keywords)
+{
+  return fetch("https://www.google.com/search?tbm=isch&q=" +
+               encodeURIComponent(keywords + " " + topic))
+    .then(response => response.text())
+    .then(source =>
+    {
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(source, "text/html");
+
+      return new Promise((resolve, reject) =>
+      {
+        let image = new Image();
+        image.addEventListener("load", () =>
+        {
+          resolve(image);
+        });
+        image.addEventListener("error", () =>
+        {
+          let thumbnail = new Image();
+          thumbnail.addEventListener("load", () =>
+          {
+            resolve(thumbnail);
+          });
+          thumbnail.addEventListener("error", reject);
+          thumbnail.src = doc.querySelector("#search img").src;
+        });
+        image.src = JSON.parse(doc.querySelector(".rg_meta").textContent).ou;
+      });
+    });
+}
+
+function loadImageCached(keywords)
+{
+  let promise = cache.get(keywords);
+
+  if (!promise)
+  {
+    promise = loadImage(keywords).catch(error =>
+    {
+      cache.delete(keywords);
+      throw error;
+    });
+
+    while (cache.size >= CACHE_SIZE)
+      cache.delete(cache.keys().next().value);
+  }
+  else
+  {
+    cache.delete(keywords);
+  }
+  cache.set(keywords, promise);
+
+  return promise;
+}
+
+function getScaledImage(keywords, width, height)
+{
+  return loadImageCached(keywords).then(
+    image =>
+    {
+      let dWidth = width;
+      let dHeight = height;
+      let ratio = image.width / image.height;
+      if (ratio > dWidth / dHeight)
+        dHeight = dWidth / ratio;
+      else
+        dWidth = dHeight * ratio;
+
+      let canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext("2d").drawImage(
+        image,
+        0,
+        0,
+        image.width,
+        image.height,
+        (width - dWidth) / 2,
+        (height - dHeight) / 2,
+        dWidth,
+        dHeight
+      );
+
+      return canvas.toDataURL("image/png");
+    },
+    () => null
+  );
+}
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) =>
 {
   if (whitelist.has(new URL(sender.tab.url).host))
@@ -12,82 +102,7 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) =>
     return false;
   }
 
-  let promise = cache.get(msg.keywords);
-  if (!promise)
-  {
-    promise = fetch("https://www.google.com/search?tbm=isch&q=" +
-                    encodeURIComponent(msg.keywords + " " + topic))
-      .then(response => response.text())
-      .then(source =>
-      {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(source, "text/html");
-
-        return new Promise((resolve, reject) =>
-        {
-          let image = new Image();
-          image.addEventListener("load", () =>
-          {
-            resolve(image);
-          });
-          image.addEventListener("error", () =>
-          {
-            let thumbnail = new Image();
-            thumbnail.addEventListener("load", () =>
-            {
-              resolve(thumbnail);
-            });
-            thumbnail.addEventListener("error", reject);
-            thumbnail.src = doc.querySelector("#search img").src;
-          });
-          image.src = JSON.parse(doc.querySelector(".rg_meta").textContent).ou;
-        });
-      })
-      .catch(error =>
-      {
-        cache.delete(msg.keywords);
-        throw error;
-      });
-
-    while (cache.size >= CACHE_SIZE)
-      cache.delete(cache.keys().next().value);
-  }
-  else
-  {
-    cache.delete(msg.keywords);
-  }
-  cache.set(msg.keywords, promise);
-
-  promise
-    .then(image =>
-    {
-      let {width, height} = msg;
-      let ratio = image.width / image.height;
-      if (ratio > width / height)
-        height = width / ratio;
-      else
-        width = height * ratio;
-
-      let canvas = document.createElement("canvas");
-      canvas.width = msg.width;
-      canvas.height = msg.height;
-      canvas.getContext("2d").drawImage(
-        image,
-        0,
-        0,
-        image.width,
-        image.height,
-        (msg.width - width) / 2,
-        (msg.height - height) / 2,
-        width,
-        height
-      );
-
-      return canvas.toDataURL("image/png");
-    })
-    .catch(() => null)
-    .then(sendResponse);
-
+  getScaledImage(msg.keywords, msg.width, msg.height).then(sendResponse);
   return true;
 });
 
